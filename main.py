0f7c42df30213b8b8aff11618b87c60ea9c5d86c
cmbak <chikmii@gmail.com>
2024-01-30 15:02:27 +0000

    Added translators library

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# import sys
 import argparse
+import translators as ts
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 
 # SAMPLE TEXTS FROM LAWLESS FRENCH https://www.lawlessfrench.com
 
-parser = argparse.ArgumentParser(description='Convert a piece of French text into Anki cards')
+parser = argparse.ArgumentParser(description='Convert a piece of French text (utf-8) into Anki cards')
 parser.add_argument('filename', nargs=1, help='name of the file to create Anki cards from') # TODO Add multiple file implementation later
 args = parser.parse_args()
 
-file = ""
+translate_to_lang = 'en'
 
 def validate_file_format(file_path):
     split_file_path = file_path.split('.')
@@ -18,14 +18,20 @@ def validate_file_format(file_path):
     return True
 
 def main_prog(filename):
+    file = ""
+
     try:
         with open(filename, encoding="utf-8") as f: # Will only get 1 file
             file = f.read()
+
+            sentences = sent_tokenize(file, language='french')
+            for s in sentences:
+                print(s, "\nTRANSLATION\n", ts.translate_text(s, translator='bing', from_language='fr', to_language=translate_to_lang), "\n===============\n")
+            
+
     except Exception as e:
         print(str(e))
         # TODO quit/throw error instead of printing?
-
-    print(file)
 
 if validate_file_format(args.filename[0]):
     main_prog(args.filename[0])
