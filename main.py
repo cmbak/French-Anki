711bf2d6ca49185520106313923bfb3efd2bf522
cmbak <chikmii@gmail.com>
2024-01-30 13:54:01 +0000

    Added command line argument parser for text file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# import sys
+import argparse
 import nltk
 from nltk.tokenize import word_tokenize
 
-#print(word_tokenize("It's over Anakin, I have the high ground!"))
-
 # SAMPLE TEXTS FROM LAWLESS FRENCH https://www.lawlessfrench.com
+
+parser = argparse.ArgumentParser(description='Convert a piece of French text into Anki cards')
+parser.add_argument('filename', nargs=1, help='name of the file to create Anki cards from') # TODO Add multiple file implementation later
+args = parser.parse_args()
+
+french_file = args.filename
+print(type(french_file))
