ef5b8fb837b7955d371f57d0ac7a173a9eab83bb
cmbak <chikmii@gmail.com>
2024-01-30 14:23:23 +0000

    File format is validated before main code is run

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ args = parser.parse_args()
 
 file = ""
 
-# Need to ensure that file is txt file!
-try:
-    with open(args.filename[0], encoding="utf-8") as f: # Will only get 1 file
-        file = f.read()
-except Exception as e:
-    print(str(e))
-    # TODO quit/throw error instead of printing?
+def validate_file_format(file_path):
+    split_file_path = file_path.split('.')
+    if len(split_file_path) != 2 or split_file_path[1] != 'txt':
+        return False
+    return True
 
-print(file)
+def main_prog(filename):
+    try:
+        with open(filename, encoding="utf-8") as f: # Will only get 1 file
+            file = f.read()
+    except Exception as e:
+        print(str(e))
+        # TODO quit/throw error instead of printing?
+
+    print(file)
+
+if validate_file_format(args.filename[0]):
+    main_prog(args.filename[0])
+else:
+    print("Please enter a valid file format (.txt)")
+
+
+# Need to ensure that file is txt file!
